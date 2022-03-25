3f4d3c0abb1a0fa18bbd7f48fff89b466fbb077d
ducciow <vvib@qq.com>
2022-03-25 20:06:59 +0100

    done with primary class

---
+++ src/primary/class08/Code33_MergeSort.java
@@ -0,0 +1,123 @@
+package primary.class08;
+
+import java.util.Arrays;
+
+/**
+ * @Author: duccio
+ * @Date: 25, 03, 2022
+ * @Description: Merge sort: recursion version (top-down)
+ *                           iteration version (bottom-up)
+ * @Note:   For iteration version, be careful about the possible index overflow
+ */
+public class Code33_MergeSort {
+    public static void main(String[] args) {
+        goValidate();
+    }
+
+    public static void merge(int[] arr, int L, int M, int R) {
+        int[] tmp = new int[R - L + 1];
+        int i = 0;
+        int p1 = L;
+        int p2 = M + 1;
+        while (p1 <= M && p2 <= R) {
+            tmp[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= M) {
+            tmp[i++] = arr[p1++];
+        }
+        while (p2 <= R) {
+            tmp[i++] = arr[p2++];
+        }
+        for (int j = 0; j < tmp.length; j++) {
+            arr[L + j] = tmp[j];
+        }
+    }
+
+    public static void mergeSort1(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process(arr, 0, arr.length - 1);
+    }
+
+    public static void process(int[] arr, int L, int R) {
+        if (L == R) {
+            return;
+        }
+        int M = L + ((R - L) >> 1);
+        process(arr, L, M);
+        process(arr, M + 1, R);
+        merge(arr, L, M, R);
+    }
+
+    public static void mergeSort2(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int N = arr.length;
+        int step = 1;
+        while (step < N) {
+            int L = 0;
+            while (L < N) {
+                if (step >= N - L) {  // avoid overflow
+                    break;
+                }
+                int M = L + step - 1;
+                int R = M + Math.min(step, N - 1 - M);
+                merge(arr, L, M, R);
+                L = R + 1;
+            }
+
+            if (step > N >> 1) {  // avoid overflow
+                break;
+            }
+            step <<= 1;
+        }
+    }
+
+
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if (arr1 == null ^ arr2 == null) {
+            return false;
+        }
+        if (arr1 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void goValidate() {
+        int testTime = 50000;
+        int maxSize = 100;
+        int maxValue = 100;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = Arrays.copyOf(arr1, arr1.length);
+            mergeSort2(arr1);
+            Arrays.sort(arr2);
+            if (!isEqual(arr1, arr2)) {
+                System.out.println("Failed on case: ");
+                System.out.println(Arrays.toString(arr1));
+                System.out.println(Arrays.toString(arr2));
+                return;
+            }
+        }
+        System.out.println("Test passed!");
+    }
+}
