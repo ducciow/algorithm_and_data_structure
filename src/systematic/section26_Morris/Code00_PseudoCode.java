23e1100ad19454fbd7c72690cdbf5cb1fc9362f3
ducciow <vvib@qq.com>
2022-05-12 12:46:38 +0200

    .

---
+++ src/systematic/section26_Morris/Code00_PseudoCode.java
@@ -0,0 +1,39 @@
+package systematic.section26_Morris;
+
+/**
+ * @Author: duccio
+ * @Date: 11, 05, 2022
+ * @Description: Pseudo code of Morris traversal.
+ * @Note:   1. Time complexity O(N).
+ *          2. Space complexity O(1).
+ *          ======
+ *          1. Those nodes with left child are traversed twice, otherwise are traversed once.
+ *          2. In general for applications, further operations take place on the twice traversing.
+ *          ======
+ *
+ *                                                  Morris      Binary Tree DP
+ *          if require entire info from subtree:      no             yes
+ */
+public class Code00_PseudoCode {
+
+    /*
+
+    Assume it comes to the current node, cur:
+
+    if (cur.left != null):
+        rightMost = the rightmost node in the left subtree
+        if (rightMost.right == null):
+            rightMost.right = cur
+            cur = cur.left
+        else if (rightMost.right == cur):
+            rightMost.right = null
+            cur = cur.right
+    else:
+        cur = cur.right
+
+    if (cur == null):
+        stop iterating
+
+     */
+
+}
---
+++ src/systematic/section26_Morris/Code01_MorrisTraversal.java
@@ -0,0 +1,202 @@
+package systematic.section26_Morris;
+
+/**
+ * @Author: duccio
+ * @Date: 12, 05, 2022
+ * @Description: Implement Morris traversal, and modify it to pre-, in-, post-traversal. Also, use Morris for IsBST.
+ * @Note:   1. Morris to Pre: print at the first time all nodes are visited.
+ *          2. Morris to In: print when visit-once nodes are visited and visit-twice nodes are visited the second time.
+ *          3. Morris to Post: only print when visit-twice nodes are visited the second time, which reversely print the
+ *             right boundary of its left subtree, and at the end, print the right boundary of root.
+ *          4. Morris to IsBST: check if previous node is larger than current node before whenever current goes to its
+ *             right, and revert to the original tree before return.
+ */
+public class Code01_MorrisTraversal {
+
+    public static void main(String[] args) {
+        Node root = new Node(4);
+        root.left = new Node(2);
+        root.right = new Node(6);
+        root.left.left = new Node(1);
+        root.left.right = new Node(3);
+        root.right.left = new Node(5);
+        root.right.right = new Node(7);
+        morris(root);
+        morrisPre(root);
+        morrisIn(root);
+        morrisPost(root);
+        System.out.println(isBST(root));
+    }
+
+    public static class Node {
+        int val;
+        Node left;
+        Node right;
+
+        public Node(int v) {
+            val = v;
+        }
+    }
+
+    public static void morris(Node root) {
+        if (root == null) {
+            return;
+        }
+        Node cur = root;
+        Node rightMost = null;
+        while (cur != null) {
+            System.out.print(cur.val + " ");
+            rightMost = cur.left;
+            if (rightMost != null) {
+                while (rightMost.right != null && rightMost.right != cur) {
+                    rightMost = rightMost.right;
+                }
+                if (rightMost.right == null) {
+                    rightMost.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    rightMost.right = null;
+                }
+            }
+            cur = cur.right;
+        }
+        System.out.println();
+    }
+
+    public static void morrisPre(Node root) {
+        if (root == null) {
+            return;
+        }
+        Node cur = root;
+        Node rightMost = null;
+        while (cur != null) {
+            rightMost = cur.left;
+            if (rightMost != null) {
+                while (rightMost.right != null && rightMost.right != cur) {
+                    rightMost = rightMost.right;
+                }
+                if (rightMost.right == null) {
+                    System.out.print(cur.val + " ");
+                    rightMost.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    rightMost.right = null;
+                }
+            } else {
+                System.out.print(cur.val + " ");
+            }
+            cur = cur.right;
+        }
+        System.out.println();
+    }
+
+    public static void morrisIn(Node root) {
+        if (root == null) {
+            return;
+        }
+        Node cur = root;
+        Node rightMost = null;
+        while (cur != null) {
+            rightMost = cur.left;
+            if (rightMost != null) {
+                while (rightMost.right != null && rightMost.right != cur) {
+                    rightMost = rightMost.right;
+                }
+                if (rightMost.right == null) {
+                    rightMost.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    rightMost.right = null;
+                }
+            }
+            System.out.print(cur.val + " ");
+            cur = cur.right;
+        }
+        System.out.println();
+    }
+
+    private static void morrisPost(Node root) {
+        if (root == null) {
+            return;
+        }
+        Node cur = root;
+        Node rightMost = null;
+        while (cur != null) {
+            rightMost = cur.left;
+            if (rightMost != null) {
+                while (rightMost.right != null && rightMost.right != cur) {
+                    rightMost = rightMost.right;
+                }
+                if (rightMost.right == null) {
+                    rightMost.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    rightMost.right = null;
+                    reversePrintRightBoundary(cur.left);
+                }
+            }
+            cur = cur.right;
+        }
+        reversePrintRightBoundary(root);
+        System.out.println();
+    }
+
+    private static void reversePrintRightBoundary(Node node) {
+        Node tail = reverse(node);
+        Node cur = tail;
+        while (cur != null) {
+            System.out.print(cur.val + " ");
+            cur = cur.right;
+        }
+        reverse(tail);
+    }
+
+    public static Node reverse(Node node) {
+        Node pre = null;
+        Node next = null;
+        while (node != null) {
+            next = node.right;
+            node.right = pre;
+            pre = node;
+            node = next;
+        }
+        return pre;
+    }
+
+    public static boolean isBST(Node root) {
+        if (root == null) {
+            return true;
+        }
+        Node cur = root;
+        Node rightMost = null;
+        boolean ans = true;
+        Node pre = null;
+        while (cur != null) {
+            rightMost = cur.left;
+            if (rightMost != null) {
+                while (rightMost.right != null && rightMost.right != cur) {
+                    rightMost = rightMost.right;
+                }
+                if (rightMost.right == null) {
+                    rightMost.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    rightMost.right = null;
+                }
+            }
+            if (pre != null && pre.val > cur.val) {
+                ans = false;
+            }
+            pre = cur;
+            cur = cur.right;
+        }
+        return ans;
+    }
+
+
+}
