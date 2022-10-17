b5483dbc767d33426ba4c8f50d7dc3e7eb6d5543
ducciow <vvib@qq.com>
2022-10-17 22:18:04 +0800

    review

--- src/systematic/section17_BruteForcingRecursion/Code04_ReverseStackUsingRecursive.java
+++ src/systematic/section17_BruteForcingRecursion/Code04_ReverseStackUsingRecursive.java
@@ -6,13 +6,14 @@ import java.util.Stack;
  * @Author: duccio
  * @Date: 21, 04, 2022
  * @Description: Reversing a stack using recursion solely, ie. no extra data structures.
- * @Note:   - Key idea:
- *              recursively get the bottom element and push it back.
- *          - To reverse, define reverse(), recursively:
+ * @Note:   a) Key idea:
+ *              - recursively get the bottom element and push it back.
+ *              - the deciding feature is actually the stack mechanism behind recursion in Java.
+ *          b) To reverse, define reverse(), recursively:
  *              1. get and keep the bottom item.
  *              2. reverse elements above by calling itself.
  *              3. push the bottom back.
- *          - To get the bottom item, define bottom(), which recursively:
+ *          c) To get the bottom item, define bottom(), which recursively:
  *              1. get and keep the top item.
  *              2. get the bottom item by calling itself.
  *              3. push the top back.
