5c828fa6cb10f74f06ba9b551748b71f15bec822
ducciow <vvib@qq.com>
2023-01-09 22:34:11 +0800

    review

--- src/section18_DynamicProgramming/Code20_NQueens.java
+++ src/section18_DynamicProgramming/Code20_NQueens.java
@@ -3,27 +3,13 @@ package section18_DynamicProgramming;
 /**
  * @Author: duccio
  * @Date: 02, 05, 2022
- * @Description: N-Queens problem
+ * @Description: N-Queens problem.
  * @Note:   Ver1. naive brute force.
  *          Ver2. use bit representation reducing constant operation time.
  *          ======
- *          N-Queens cannot be converted to DP.
+ *          - N-Queens cannot be converted to DP.
  */
 public class Code20_NQueens {
-
-    public static void main(String[] args) {
-        int n = 13;
-
-        long start = System.currentTimeMillis();
-        System.out.println(ways1(n));
-        long end = System.currentTimeMillis();
-        System.out.println("cost time: " + (end - start) + "ms");
-
-        start = System.currentTimeMillis();
-        System.out.println(ways2(n));
-        end = System.currentTimeMillis();
-        System.out.println("cost time: " + (end - start) + "ms");
-    }
 
     public static int ways1(int N) {
         if (N < 1) {
@@ -67,28 +53,52 @@ public class Code20_NQueens {
 
     /**
      * @param limit      eg., 00...011111 means 5-Queens problem.
-     * @param colX       column restriction from previous rows
-     * @param leftDiagX  left diagonal restriction from previous rows
-     * @param rightDiagX right diagonal restriction from previous rows
-     * @return number of ways
+     * @param colX       column restriction from previous rows.
+     * @param leftDiagX  left diagonal restriction from previous rows.
+     * @param rightDiagX right diagonal restriction from previous rows.
+     * @return number of ways.
      */
     public static int process2(int limit, int colX, int leftDiagX, int rightDiagX) {
         if (colX == limit) {
             return 1;
         }
         int ans = 0;
-        // digit 1 in pos means valid
-        int pos = limit & (~(colX | leftDiagX | rightDiagX));
+        // 1 in validPos means valid positions
+        int validPos = limit & (~(colX | leftDiagX | rightDiagX));
         int mostRightOne = 0;
-        while (pos != 0) {
-            mostRightOne = pos & (~pos + 1);
-            pos -= mostRightOne;
+        while (validPos != 0) {
+            // pick the rightmost valid position
+            mostRightOne = validPos & (~validPos + 1);
+            // mark that position accordingly
+            validPos -= mostRightOne;
             int colX_ = colX | mostRightOne;
             int leftDiagX_ = (leftDiagX | mostRightOne) << 1;
             int rightDiagX_ = (rightDiagX | mostRightOne) >>> 1;
             ans += process2(limit, colX_, leftDiagX_, rightDiagX_);
         }
         return ans;
+    }
+
+    public static void main(String[] args) {
+        int n = 13;
+
+        long start = System.currentTimeMillis();
+        System.out.println(ways1(n));
+        long end = System.currentTimeMillis();
+        System.out.println("cost time: " + (end - start) + "ms");
+
+        start = System.currentTimeMillis();
+        System.out.println(ways2(n));
+        end = System.currentTimeMillis();
+        System.out.println("cost time: " + (end - start) + "ms");
+
+    }
+
+    public static void printBinary(int num) {
+        for (int i = 31; i >= 0; i--) {
+            System.out.print((num & (1 << i)) == 0 ? "0" : "1");
+        }
+        System.out.println();
     }
 
 }
